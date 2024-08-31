1285225be0a5068079ad4f1f6c5efbc4774a4bbf
Jorge Trejo <jorgeetrejoo@gmail.com>
2024-08-30 21:58:31 -0600

    first commit

---
+++ constants/seo.ts
@@ -0,0 +1,5 @@
+export const SEO = {
+  title: "PostulaNet",
+  description:
+    "Es una plataforma innovadora de gestión de vacantes y postulaciones, diseñada para empresas que buscan candidatos altamente comprometidos y para postulantes que desean destacar en el competitivo mercado laboral. A través de un sistema de puntos internos, las empresas podrán asignar un valor a cada vacante, lo que permitirá a los postulantes optar por convertirse en candidatos premium al pagar dichos puntos. Este enfoque asegura que las empresas reciban postulaciones de candidatos realmente interesados y calificados, mientras que los postulantes premium obtienen una mayor visibilidad y prioridad en el proceso de selección.",
+};
